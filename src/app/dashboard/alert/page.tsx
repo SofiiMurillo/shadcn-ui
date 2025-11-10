73f4f9bafd92b0a0f4727de8fd30f56cd9c88290
Sofia <sofimurillolopez7@gmail.com>
2025-11-10 18:10:21 -0500

    cuarto avance

--- src/app/dashboard/alert/page.tsx
+++ src/app/dashboard/alert/page.tsx
@@ -1,7 +1,49 @@
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { AlertCircleIcon, CheckCircle2Icon, PopcornIcon, Star } from "lucide-react";
+
 export default function page() {
     return(
-        <div>
-            <h1>Alert Page</h1>
+        <div className="grid gap-3" >
+
+            <Alert>
+        <CheckCircle2Icon />
+        <AlertTitle>Success! Your changes have been saved</AlertTitle>
+        <AlertDescription>
+          This is an alert with icon, title and description.
+        </AlertDescription>
+      </Alert>
+      <Alert>
+        <PopcornIcon />
+        <AlertTitle>
+          This Alert has a title and an icon. No description.
+        </AlertTitle>
+      </Alert>
+      <Alert variant="destructive">
+        <AlertCircleIcon />
+        <AlertTitle>Unable to process your payment.</AlertTitle>
+        <AlertDescription>
+          <p>Please verify your billing information and try again.</p>
+          <ul className="list-inside list-disc text-sm">
+            <li>Check your card details</li>
+            <li>Ensure sufficient funds</li>
+            <li>Verify billing address</li>
+          </ul>
+        </AlertDescription>
+      </Alert>
+
+      <Alert variant="success">
+        <Star />
+        <AlertTitle>Todo bien </AlertTitle>
+        <AlertDescription>
+          <p>lo hicimos</p>
+          <ul className="list-inside list-disc text-sm">
+            <li>Check your card details</li>
+            <li>Ensure sufficient funds</li>
+            <li>Verify billing address</li>
+          </ul>
+        </AlertDescription>
+      </Alert>
+            
         </div>
     )
 }
---
+++ src/app/dashboard/button/page.tsx
@@ -0,0 +1,80 @@
+"use client"
+
+import { Button } from "@/components/ui/button";
+import { ArrowUpIcon, CircleFadingArrowUpIcon } from "lucide-react";
+
+
+function Spinner() {
+  return (
+    <svg
+      className="animate-spin h-4 w-4 mr-2 text-current"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      aria-hidden="true"
+    >
+      <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+      <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8v4a4 4 0 00-4 4H4z"></path>
+    </svg>
+  );
+}
+
+
+
+export default function page() {
+    return(
+        <div className="grid grid-cols-5 gap-2" >
+
+
+      <Button>
+        Default
+      </Button>
+        
+      <Button variant={"destructive"}>
+        Destructive
+      </Button>
+
+      <Button variant={"ghost"}>
+        Ghost
+      </Button>
+
+      <Button variant={"link"}>
+        Link
+      </Button>
+
+      <Button variant={"outline"}>
+        Outline
+      </Button>
+
+      <Button variant={"secondary"}>
+        Secondary
+      </Button>
+
+      <Button onClick={() => console.log("Mensaje en consola :D")}>
+        Click Me :3
+      </Button>
+      
+      <Button variant={"success"}>
+        ¡Lo grado!
+      </Button>
+            
+      <Button variant={"outline" } size="icon" >
+        <CircleFadingArrowUpIcon />
+       
+      </Button>
+
+
+
+      <Button variant={"outline" } size="icon" className="rounded-full" >
+        <ArrowUpIcon />
+      </Button>
+
+<Button size="sm" variant="outline" disabled>
+  <Spinner />
+  Submit
+</Button>
+
+            
+        </div>
+    )
+}
